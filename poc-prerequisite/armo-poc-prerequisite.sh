110c8e44f4863dcc39849f9a63b1feec385ee6d5
David Alima <davida@armosec.io>
2024-10-22 21:02:40 +0300

    remove commenting
    Signed-off-by: David Alima <davida@armosec.io>

--- poc-prerequisite/armo-poc-prerequisite.sh
+++ poc-prerequisite/armo-poc-prerequisite.sh
@@ -81,7 +81,7 @@ verify_helm_permissions() {
   CLUSTER_NAME=$(kubectl config current-context)
   local HELM_OUTPUT
   
-HELM_OUTPUT=$( { helm repo add "$RELEASE_NAME" "$HELM_REPO" && \
+  HELM_OUTPUT=$( { helm repo add "$RELEASE_NAME" "$HELM_REPO" && \
     helm repo update && \
     helm upgrade --install --dry-run "$RELEASE_NAME" "$CHART_NAME" -n "$NAMESPACE" --create-namespace \
     --set clusterName="$CLUSTER_NAME" \
